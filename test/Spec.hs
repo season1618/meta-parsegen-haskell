8ca1a102f8837edd93fce0c1c91a8ae6fa9f7e60
season1618 <seasonminority314@gmail.com>
2024-03-12 22:55:38 +0900

    expression test

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,10 +6,22 @@
 import Parser
 import GHC.Generics
 
-data Expr = Add Int (StrLit "+") Expr
+data Expr = Add Term (StrLit "+") Expr
+          | Sub Term (StrLit "-") Expr
+          | Term Term
+    deriving (Show, Generic, Parser)
+
+data Term = Mul Fact (StrLit "*") Term
+          | Div Fact (StrLit "/") Term
+          | Fact Fact
+    deriving (Show, Generic, Parser)
+
+data Fact = Expr (StrLit "(") Expr (StrLit ")")
           | Num Int
     deriving (Show, Generic, Parser)
 
 main :: IO ()
 main = do
-    print $ (parse "123+28+35" :: Maybe (Expr, String))
+    print $ (parse "1+2-3" :: Maybe (Expr, String))
+    print $ (parse "4/5*6" :: Maybe (Expr, String))
+    print $ (parse "(1+2)*(3/4)-5" :: Maybe (Expr, String))
